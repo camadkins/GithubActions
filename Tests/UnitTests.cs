25a8ef23c9e9fcbc98574296034ed383a4b6ea37
camadkins <48412231+camadkins@users.noreply.github.com>
2026-03-25 22:28:12 -0500

    Add tests with a failure

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -6,7 +6,7 @@ public class Addition
 	[TestMethod]
 	public void Add_Valid_Adkins()
 	{
-		Assert.AreEqual(3, Program.Add("1", "2"));
+		Assert.AreEqual(4, Program.Add("1", "2"));
 		Assert.AreEqual(5, Program.Add("3", "2"));
 		Assert.AreEqual(12, Program.Add("5", "7"));
 	}
@@ -26,4 +26,116 @@ public class Addition
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
 	}
+}
+
+  [TestClass]
+  public class Subtraction
+  {
+      [TestMethod]
+      public void Subtract_Valid_Adkins()
+      {
+          Assert.AreEqual(1, Program.Subtract("3", "2"));
+          Assert.AreEqual(-1, Program.Subtract("2", "3"));
+          Assert.AreEqual(0, Program.Subtract("5", "5"));
+      }
+
+      [TestMethod]
+      public void Subtract_Invalid_Adkins()
+      {
+          Assert.ThrowsException<FormatException>(() => Program.Subtract("1", "a"));
+          Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "1"));
+          Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "a"));
+      }
+
+      [TestMethod]
+      public void Subtract_Null_Adkins()
+      {
+          Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract("1", null));
+          Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, "1"));
+          Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, null));
+      }
+  }
+
+  [TestClass]
+  public class Multiplication
+  {
+      [TestMethod]
+      public void Multiply_Valid_Adkins()
+      {
+          Assert.AreEqual(6, Program.Multiply("2", "3"));
+          Assert.AreEqual(0, Program.Multiply("0", "5"));
+          Assert.AreEqual(35, Program.Multiply("5", "7"));
+      }
+
+      [TestMethod]
+      public void Multiply_Invalid_Adkins()
+      {
+          Assert.ThrowsException<FormatException>(() => Program.Multiply("1", "a"));
+          Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "1"));
+          Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "a"));
+      }
+
+      [TestMethod]
+      public void Multiply_Null_Adkins()
+      {
+          Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply("1", null));
+          Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, "1"));
+          Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, null));
+      }
+  }
+
+  [TestClass]
+  public class Division
+  {
+      [TestMethod]
+      public void Divide_Valid_Adkins()
+      {
+          Assert.AreEqual(2, Program.Divide("6", "3"));
+          Assert.AreEqual(2.5, Program.Divide("5", "2"));
+          Assert.AreEqual(1, Program.Divide("7", "7"));
+      }
+
+      [TestMethod]
+      public void Divide_Invalid_Adkins()
+      {
+          Assert.ThrowsException<FormatException>(() => Program.Divide("1", "a"));
+          Assert.ThrowsException<FormatException>(() => Program.Divide("a", "1"));
+          Assert.ThrowsException<FormatException>(() => Program.Divide("a", "a"));
+      }
+
+      [TestMethod]
+      public void Divide_Null_Adkins()
+      {
+          Assert.ThrowsException<ArgumentNullException>(() => Program.Divide("1", null));
+          Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, "1"));
+          Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, null));
+      }
+  }
+
+  [TestClass]
+  public class PowerTest
+  {
+      [TestMethod]
+      public void Power_Valid_Adkins()
+      {
+          Assert.AreEqual(8, Program.Power("2", "3"));
+          Assert.AreEqual(1, Program.Power("5", "0"));
+          Assert.AreEqual(25, Program.Power("5", "2"));
+      }
+
+      [TestMethod]
+      public void Power_Invalid_Adkins()
+      {
+          Assert.ThrowsException<FormatException>(() => Program.Power("1", "a"));
+          Assert.ThrowsException<FormatException>(() => Program.Power("a", "1"));
+          Assert.ThrowsException<FormatException>(() => Program.Power("a", "a"));
+      }
+
+  [TestMethod]
+  public void Power_Null_Adkins()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Power("1", null));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, "1"));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, null));
+    }
 }
